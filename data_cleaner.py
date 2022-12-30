01f2d4063e6014313bdf665b447cbbe92454aa3e
AnujAgrawal30 <anujagrawal.official@gmail.com>
2022-12-30 15:45:23 +0530

    fine tuning

--- data_cleaner.py
+++ data_cleaner.py
@@ -8,15 +8,4 @@ df.set_index('Date case visited -1', inplace=True)
 daily_count = df.groupby(pd.Grouper(freq='D')).size()
 daily_count = daily_count.resample('D').last().fillna(0)
 
-
-# Group the data by the date column and compute the size of each group
-# daily_count = df.groupby(['Date case visited -1']).size()
-
-# Generate a sequence of dates to use as the new index
-# dates = pd.date_range(start=daily_count.index.min(), end=daily_count.index.max())
-
-# Reindex the daily count with the new dates
-# daily_count = daily_count.reindex(index=dates, fill_value=0)
-
-# Print the resulting daily count
 print(list(daily_count))
